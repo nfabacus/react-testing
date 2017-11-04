b062b191ab36cc28cce05af63199c158aa6ec6f0
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-11-04 11:19:37 +0000

    connect up CommentReducert to rootReducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import CommentsReducer from './comments'
 
 const rootReducer = combineReducers({
-  comments: (state = []) => state
+  comments: CommentsReducer
 });
 
 export default rootReducer;
